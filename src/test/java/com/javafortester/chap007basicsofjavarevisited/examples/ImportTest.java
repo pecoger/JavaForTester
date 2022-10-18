2b6c2b05acc15d540f3fd619a1c717554104a708
pecoger <peco20@abv.bg>
2022-10-18 17:31:14 +0300

    finished chap 7

--- src/test/java/com/javafortester/chap007basicsofjavarevisited/examples/ImportTest.java
+++ src/test/java/com/javafortester/chap007basicsofjavarevisited/examples/ImportTest.java
@@ -1,6 +1,33 @@
 package com.javafortester.chap007basicsofjavarevisited.examples;
 
-//import
+import com.javafortester.chap001.examples.classes.*;
+import com.javafortester.domainentities.User;
+import org.junit.Test;
+
+import org.junit.Assert;
+import static org.junit.Assert.assertEquals;
 
 public class ImportTest {
+
+    @org.junit.Test
+    public void nonImportTest(){
+        org.junit.Assert.assertEquals(3, 2 + 1);
+    }
+
+    @Test
+    public void importClassNameTest(){
+        User user = new User();
+    }
+
+    @Test
+    public void importClassWithWildcard(){
+        AClassWithAMethod aClass = new AClassWithAMethod();
+        AnEmptyClass emptyClass = new AnEmptyClass();
+    }
+
+    @Test
+    public void nonStaticImport(){
+        Assert.assertEquals(5, 2 + 3);
+        assertEquals(6, 4 + 2);
+    }
 }
---
+++ src/test/java/com/javafortester/chap007basicsofjavarevisited/examples/StringsExampleTest.java
@@ -0,0 +1,26 @@
+package com.javafortester.chap007basicsofjavarevisited.examples;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class StringsExampleTest {
+
+    @Test
+    public void stringsConcatenated(){
+        assertEquals("123456", "12" + "34" + "56");
+    }
+
+    @Test
+    public void someStringMethods(){
+        String aString = "abcdef";
+
+        assertEquals(6, aString.length());
+        assertTrue(aString.compareToIgnoreCase("ABCDEF")==0);
+        assertTrue(aString.contains("bcd"));
+        assertTrue(aString.startsWith("abc"));
+
+        // string indexing starts at 0
+        assertEquals('c', aString.charAt(2));
+        assertEquals("ef", aString.substring(4));
+    }
+}
